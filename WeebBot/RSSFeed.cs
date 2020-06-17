9a03ccca687811ba8d42ffff94c60312ea68099b
Awcmon <Awcmon@users.noreply.github.com>
2020-06-16 20:54:56 -0700

    Initial commit

---
+++ WeebBot/RSSFeed.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace WeebBot
+{
+	class RSSFeed
+	{
+		public string FeedUrl { get; private set; }
+		public SyndicationFeed Feed { get; private set; }
+
+		public RSSFeed(string feedUrl)
+		{
+			FeedUrl = feedUrl;
+			Read();
+		}
+
+		public void Read()
+		{
+			XmlReader reader = XmlReader.Create(FeedUrl);
+			Feed = SyndicationFeed.Load(reader);
+			reader.Close();
+		}
+
+		public void PrintFeedItems(Object stateInfo)
+		{
+			foreach (SyndicationItem item in Feed.Items)
+			{
+				String subject = item.Title.Text;
+				String summary = item.Summary.Text;
+				DateTimeOffset date = item.PublishDate;
+				Console.WriteLine($"{subject}\n{summary}\n{date}");
+				foreach (SyndicationLink l in item.Links)
+				{
+					Console.WriteLine($"{l.GetAbsoluteUri()}");
+				}
+				Console.WriteLine($"\n");
+			}
+		}
+
+	}
+}
---
+++ WeebBot/Services/NotificationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace WeebBot.Services
+{
+	public class NotificationService
+	{
+		private readonly DiscordSocketClient _discord;
+		private readonly IServiceProvider _services;
+
+		public NotificationService(IServiceProvider services)
+		{
+			_discord = services.GetRequiredService<DiscordSocketClient>();
+			_services = services;
+
+			List<RSSFeed> feeds = new List<RSSFeed>();
+			List<Timer> timers = new List<Timer>();
+
+			feeds.Add(new RSSFeed("https://mangadex.org/rss/BEWhGNQMDVpTU4CznK9Hskwfu52Pegva/manga_id/31915"));
+
+			foreach(RSSFeed f in feeds)
+			{
+				timers.Add(new Timer(f.PrintFeedItems, null, 1000, 1000));
+			}
+		}
+
+
+	}
+}
